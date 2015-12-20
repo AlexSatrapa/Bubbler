8e129acfd1e2ad5480413d3476b8ae803cd653c7
Alex Satrapa <alex@arthur.private>
2015-12-21 03:44:28 +1100

    Starting to port to ATTiny841.

--- pins.h
+++ pins.h
@@ -1,6 +1,26 @@
 #ifndef PINS
 #define PINS
 
+#if defined( __AVR_ATtinyX41__ )
+// Pin 0 - XTAL
+// Pin 1 - XTAL
+// Pin 2
+#define XBEE_ENABLE 2
+// Pin 3
+#define MOTOR 3
+// Pin 4 - MOSI
+// Pin 5 - MISO
+// Pin 6 - SCK
+// Pin 7
+#define PRESSURE 7
+// Pin 8 - RX0
+#define RTC_ALARM_PIN 8
+// Pin 9 - TX0
+// Pin 10
+#define RTC_SS_PIN 10
+// Pin 11 - RESET
+
+#else
 // DIGITAL I/O
 
 // Pin 0 UART RX, PCINT16
@@ -8,39 +28,23 @@
 // Pin 2 interrupt 0, PCINT18
 #define RTC_ALARM_PIN 2
 // Pin 3 interrupt 1, PCINT19, OC2B, PWM output
-#define MOTOR 3
 
 // Pin 4 PCINT20/XCK/T0
 #define WARNLED 4
 // Pin 5 PCINT21/OC0B/T1, PWM
 #define XBEE_ENABLE 5
 // Pin 6 PCINT22/OC0A/AIN0, PWM
+#define MOTOR 6
 // Pin 7 PCINT23/AIN1
 #define PRESSURE 7
 // Pin 8 PCINT0/CLK0/ICP1
 #define RTC_SS_PIN 8
 // Pin 9 PCINT1/OC1A, PWM
-
-inline void disablePin( int pin ) {
-	pinMode( pin, INPUT );
-	digitalWrite( pin, HIGH );
-	}
-
-inline void setupPins() {
-	// Now configure the pins we're actually using
-	disablePin( 1 );
-	disablePin( 2 );
-	pinMode(MOTOR, OUTPUT);
-	pinMode(XBEE_ENABLE, OUTPUT);
-	digitalWrite(XBEE_ENABLE, LOW);
-	pinMode(WARNLED, OUTPUT);
-	digitalWrite(PRESSURE, HIGH);
-	pinMode(PRESSURE, OUTPUT);
-	digitalWrite(RTC_SS_PIN, HIGH);
-	pinMode(RTC_SS_PIN, OUTPUT);
-	disablePin( 9 );
-	}
-
+// Pin 10 default SS
+// Pin 11 MOSI
+// Pin 12 MISO
+// Pin 13 SCK, LED
+#endif
 #endif
 
 // PWM notes: http://playground.arduino.cc/Main/TimerPWMCheatsheet
